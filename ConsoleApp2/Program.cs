79783e7a8323c1af21301fcadb1be9d3b68ebdf2
swatishinde09 <148969831+swatishinde09@users.noreply.github.com>
2023-11-24 15:39:41 +0530

    Initial commit

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        public void display()
+        {
+            Console.WriteLine("Hello Jenkins");
+        }
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.display();
+            Console.ReadLine();
+        }
+    }
+}
